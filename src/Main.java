c5c955e5d0927b4eaf7e1f35d01f35dc6c4ad74c
Noah <scott.613865@swl.k12.oh.us>
2023-10-31 09:20:13 -0400

    hi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        for(int g = 5; g >= 1; g--){
+            System.out.println();
+            for(int k = g; k <= 5; k++){
+                System.out.print("*");
+            }
+        }
+        System.out.println();
+        for(int l = 5; l >= 1; l--){
+            System.out.println();
+            for(int m = l; m >= 1; m--){
+                System.out.print("*");
+            }
+        }
+        System.out.println();
+        for (int i = 5; i >= 1; i-- ){
+            System.out.println();
+            for(int j = 1; j <= 5; j++){
+                System.out.print("*");
+            }
+        }
     }
 }
